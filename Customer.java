68a76de21deb8e5dc7fc36862a5f9ede91bd9210
Mohammed Shakeel <nmohammedshakeel22@gmail.com>
2026-03-24 11:00:07 +0530

    updated

---
+++ Customer.java
@@ -0,0 +1,73 @@
+package Hexaware;
+
+public class Customer {
+    private int accountNumber;
+    private String name;
+    private double balance;
+    private String accountType;
+    private String email;
+
+    public Customer(int accountNumber, String accountType, double balance, String name, String email) {
+        this.email = email;
+        this.accountType = accountType;
+        this.balance = balance;
+        this.name = name;
+        this.accountNumber = accountNumber;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient funds or invalid amount.");
+        }
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,190 @@
+package Hexaware;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        List<Customer> accounts = new ArrayList<>();
+
+        System.out.println("Account Creation:");
+        System.out.println("1. Create account");
+        System.out.println("2. Deposit amount");
+        System.out.println("3. Withdraw amount");
+        System.out.println("4. Enquiry balance");
+        System.out.println("5. Transfer amount");
+        System.out.println("6. Search customer");
+        System.out.println("7. Display all customers");
+        System.out.println("8. Exit");
+
+        boolean isNotExit = true;
+
+        while(isNotExit) {
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1: {
+                    System.out.print("\nEnter Name: ");
+                    String name = sc.nextLine();
+                    System.out.print("\nEnter Account Type: ");
+                    String acc_type = sc.nextLine();
+                    System.out.print("\nEnter Email: ");
+                    String email = sc.nextLine();
+                    System.out.print("\nEnter Account Number: ");
+                    int acc_no = sc.nextInt();
+                    System.out.print("\nEnter Balance: ");
+                    double balance = sc.nextDouble();
+
+                    Customer acc = new Customer(acc_no, name, balance, acc_type, email);
+                    accounts.add(acc);
+
+                    break;
+                }
+
+                case 2: {
+                    System.out.print("\nEnter Account Number: ");
+                    int acc_no = sc.nextInt();
+                    System.out.print("\nEnter Amount to Deposit: ");
+                    double amt = sc.nextDouble();
+
+                    boolean isAccountExist = false;
+                    for (Customer c : accounts) {
+                        if (c.getAccountNumber() == acc_no) {
+                            c.deposit(amt);
+                            isAccountExist = true;
+                        }
+                    }
+
+                    if (isAccountExist) System.out.println("Account Not Found.");
+                    break;
+                }
+
+                case 3: {
+                    System.out.print("\nEnter Account Number: ");
+                    int acc_no = sc.nextInt();
+                    System.out.print("\nEnter Amount to Withdraw: ");
+                    double amt = sc.nextDouble();
+
+                    boolean isAccountExist = false;
+                    for (Customer c : accounts) {
+                        if (c.getAccountNumber() == acc_no) {
+                            c.withdraw(amt);
+                            isAccountExist = true;
+                        }
+                    }
+
+                    if (isAccountExist) System.out.println("Account Not Found.");
+                    break;
+                }
+
+                case 4: {
+
+                    System.out.print("\nEnter Account Number: ");
+                    int acc_no = sc.nextInt();
+
+                    boolean isAccountExist = false;
+                    for (Customer c : accounts) {
+                        if (c.getAccountNumber() == acc_no) {
+                            System.out.println("Balance: " + c.getBalance());
+                            isAccountExist = true;
+                        }
+                    }
+
+                    if (isAccountExist) System.out.println("Account Not Found.");
+                    break;
+                }
+
+                case 5: {
+
+                    System.out.print("\nEnter (From) Account Number : ");
+                    int acc_no1 = sc.nextInt();
+                    System.out.print("\nEnter (To) Account Number: ");
+                    int acc_no2 = sc.nextInt();
+                    System.out.print("\nEnter Amount to Transfer: ");
+                    double amt = sc.nextDouble();
+
+                    Customer c1 = null;
+                    Customer c2 = null;
+
+                    for (Customer c : accounts) {
+                        if (c.getAccountNumber() == acc_no1) c1 = c;
+                        if (c.getAccountNumber() == acc_no2) c2 = c;
+                    }
+
+                    if (c1 == null) {
+                        System.out.println("Account 1 Not Found.");
+                    } else if (c2 == null) {
+                        System.out.println("Account 2 Not Found.");
+                    } else if (c1.getBalance() < amt) {
+                        System.out.println("Insufficient funds in account " + acc_no1);
+                    } else {
+                        c1.withdraw(amt);
+                        c2.deposit(amt);
+                        System.out.println("Successfully transferred " + amt + " from " + acc_no1 + " to " + acc_no2);
+                    }
+                    break;
+                }
+
+                case 6: {
+                    System.out.print("\nEnter Account Number to Search: ");
+                    int acc_no = sc.nextInt();
+                    Customer c1 = null;
+
+                    for (Customer c : accounts) {
+                        if (c.getAccountNumber() == acc_no) {
+                            c1 = c;
+                            break;
+                        }
+                    }
+
+                    display(c1);
+                    break;
+                }
+
+                case 7: {
+
+                    System.out.print("\nEnter Account Number: ");
+                    int acc_no = sc.nextInt();
+
+                    Customer c1 = null;
+
+                    for (Customer c : accounts) {
+                        if (c.getAccountNumber() == acc_no) c1 = c;
+                        break;
+                    }
+
+                    display(c1);
+                    break;
+                }
+
+                case 8: {
+                    isNotExit = false;
+                    break;
+                }
+
+                default: {
+                    System.out.println("Enter correct choice");
+                }
+            }
+
+        }
+
+    }
+
+    public static void display(Customer c1){
+        if (c1 != null) {
+            System.out.println("\nAccount Details");
+            System.out.printf("\n%-15s %-10s %-20s %-10s %-10s\n", "Name", "Type", "Email", "Acc No", "Balance");
+            System.out.printf("%-15s %-10s %-20s %-10d $%-10.2f\n",
+                    c1.getName(), c1.getAccountType(), c1.getEmail(), c1.getAccountNumber(), c1.getBalance());
+        } else {
+            System.out.println("Account not found.");
+        }
+
+    }
+}
